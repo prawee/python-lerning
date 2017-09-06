09f923bcb832f197df612019c85b66bb0d271981
Prawee Wongsa <prawee.w@integra8t.com>
2017-09-06 14:51:03 +0700

    Update classes object

--- classes_objects.py
+++ classes_objects.py
@@ -30,7 +30,13 @@ class Student:
     def average(self):
         return sum(self.marks) / len(self.marks)
 
+    @classmethod
+    def go_to_school():
+        print("I'm going to school.")
+
 anna = Student("Anna", "MIT")
+rolf = Student("Rolf", "Oxford")
+
 anna.marks.append(56)
 anna.marks.append(71)
-print(anna.average())
+Student.go_to_school()
