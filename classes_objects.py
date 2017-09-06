2a951144722a5fe3a0eb877ed7dec918a2340faa
Prawee Wongsa <prawee.w@integra8t.com>
2017-09-06 14:09:57 +0700

    Added classs and object

---
+++ classes_objects.py
@@ -0,0 +1,36 @@
+lottery_player_dict = {
+    'name': 'Rolf',
+    'numbers': (5, 9, 12, 3, 1, 21)
+}
+
+class LotterPlayer: 
+    def __init__(self): 
+        self.name = 'Rolf',
+        self.numbers = (5, 9, 12, 3, 1, 21)
+
+    def total(self):
+        return sum(self.numbers)
+
+player_one = LotterPlayer()
+player_one.numbers = (1, 2, 3, 6, 7, 8)
+player_two = LotterPlayer()
+
+# print(player_one.name)
+# print(player_one.total())
+
+# print(player_one.name == player_two.name)
+print(player_one.numbers == player_two.numbers)
+
+class Student: 
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.marks = []
+    
+    def average(self):
+        return sum(self.marks) / len(self.marks)
+
+anna = Student("Anna", "MIT")
+anna.marks.append(56)
+anna.marks.append(71)
+print(anna.average())
